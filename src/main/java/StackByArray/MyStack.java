311a0b096a727d66dd347a9e41b2084262a9e14e
coffee-and-tea <40965619+coffee-and-tea@users.noreply.github.com>
2022-03-03 15:59:32 -0600

    Update MyStack.java
    comment for stack

--- src/main/java/StackByArray/MyStack.java
+++ src/main/java/StackByArray/MyStack.java
@@ -4,11 +4,16 @@ public class MyStack {
     private int maxSize;
     private int top;
     private int[] myArray;
+    
     public MyStack(int stackSize){
+        // TODO: what happends if I try to create a stack size of -1?
         this.maxSize=stackSize;
+        
+        // TODO: add a comment to explain what top means for push and pop?
         this.top=0;
         this.myArray=new int[maxSize];
     }
+    
     public boolean push(int pushValue){
         
         // can this ever happen? myArray is null?
@@ -18,6 +23,8 @@ public class MyStack {
         }
         if(top==maxSize) {
             resizeArray();
+            
+            // TODO: this seems like same like of code as below, can you improve it?
             myArray[top] = pushValue;
             top=top+1;
             return true;
@@ -26,6 +33,7 @@ public class MyStack {
         top=top+1;
         return true;
     }
+    
     public void resizeArray(){
         System.out.println("Stack is full! Will resize the stack automatically");
         maxSize=maxSize*2;
@@ -35,16 +43,20 @@ public class MyStack {
         }
         myArray=tempArray;
     }
+    
 // if top value is less than 1/2 maxSize, shrink it.
     public void shrink(){
         System.out.println("Will be shrink to 1/2 size automatically");
+        
         maxSize=maxSize/2;
+        
         int[] tempArray=new int[maxSize];
         for(int i=0; i<maxSize; i++){
             tempArray[i]=myArray[i];
         }
         myArray=tempArray;
     }
+    
     public int pop(){
         if(top==0){
             System.out.println("The Stack is empty!");
@@ -54,18 +66,23 @@ public class MyStack {
         int popValue=myArray[top];
         myArray[top]=0;
 
+        // TODO: max size is 10, when are you going to shrink?
         if(top<=maxSize/2){
             shrink();
         }
         return popValue;
     }
+    
     public int count(){
+        // TODO: can this ever happen?
         if(myArray==null){
             System.out.println("The Stack is null!");
             return 0;
         }
         return top;
     }
+    
+    
     public void delete(int deletedValue){
         int[] tempArray = new int[maxSize];
         int index_delay=0;
@@ -79,12 +96,17 @@ public class MyStack {
             }
         }
         top=top-index_delay;
+        
+        // TODO: shrink is done on the old array, and later you assign the tempArray to the old one, is this actually shrinking?
         if(top<=maxSize/2){
             shrink();
         }
+        
         myArray=tempArray;
     }
+    
     public void deleteOnce(int deletedValue){
+        // TODO: any way to do this without additional space?
         int[] tempArray = new int[maxSize];
         boolean keepfinding=true;
         int j=0;
@@ -103,7 +125,9 @@ public class MyStack {
         }
         myArray=tempArray;
     }
+    
     public void showStack(){
+        // TODO: it's good to show capacity as well, so that you know whether shrink or resized worked.
         for (int i=0;i<top;i++){
             System.out.println(myArray[i]);
         }
